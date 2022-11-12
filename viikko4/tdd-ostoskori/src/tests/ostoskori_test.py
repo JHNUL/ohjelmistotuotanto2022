40e5b2b77a6cb65669a6cfeb4f5b0922a4c82a27
Juhanir <juhani.riisio@gmail.com>
2022-11-12 13:27:44 +0200

    Viikko 4 Tehtävä 5: TDD step 5

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -26,3 +26,10 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(maito)
         self.kori.lisaa_tuote(karhupumppu)
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
+
+    def test_kahden_eri_tuotteen_lisaamisen_jalkeen_hinta_on_sama_kuin_tuotteiden_hintojen_summa(self):
+        maito = Tuote("Maito", 3)
+        karhupumppu = Tuote("Karhupumppu", 15)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(karhupumppu)
+        self.assertEqual(self.kori.hinta(), 18)
